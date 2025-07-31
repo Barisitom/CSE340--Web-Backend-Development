7c05e5b20601bf1a6b7b2e71e7e1cb23044f61d4
Barisitom <Bakpee@byupathway.edu>
2025-07-31 03:03:24 +0100

    update

---
+++ controllers/invController.js
@@ -0,0 +1,19 @@
+const invModel = require("../models/inventory-model")
+const utilities = require("../utilities/")
+
+const invCont = {}
+
+invCont.buildByClassificationId = async function (req, res, next) {
+  const classification_id = req.params.classificationId
+  const data = await invModel.getInventoryByClassificationId(classification_id)
+  const grid = await utilities.buildClassificationGrid(data.rows)
+  let nav = await utilities.getNav()
+  const className = data.rows[0].classification_name
+  res.render("./inventory/classification", {
+    title: className + " vehicles",
+    nav,
+    grid,
+  })
+}
+
+module.exports = invCont
--- server.js
+++ server.js
@@ -1,43 +1,67 @@
-/* ******************************************
- * This server.js file is the primary file of the 
- * application. It is used to control the project.
- *******************************************/
-/* ***********************
- * Require Statements
- *************************/
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
-const env = require("dotenv").config()
+require("dotenv").config()
+const baseController = require("./controllers/baseController")
+const pool = require("./database")
+const utilities = require("./utilities")
+const inventoryRoute = require("./routes/inventoryRoute")
+const staticRoutes = require("./routes/static")
+
 const app = express()
-const static = require("./routes/static")
 
-
-
-/* ******************************************
- * View engine and Templates
- *******************************************/
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout")
-/* ***********************
- * Routes
- *************************/
-app.use(static)
+app.use(express.static("public"))
 
+app.use("/", staticRoutes)
+app.get("/", utilities.handleErrors ? utilities.handleErrors(baseController.buildHome) : baseController.buildHome)
+app.use("/inv", inventoryRoute)
 
-app.get("", (req, res) => {
+app.get("/", (req, res) => {
   res.render('index')
 })
-/* ***********************
- * Local Server Information
- * Values from .env (environment) file
- *************************/
-const port = process.env.PORT
-const host = process.env.HOST
 
-/* ***********************
- * Log statement to confirm server operation
- *************************/
+app.get("/index", (req, res) => {
+  res.render('index')
+})
+
+app.get("layouts/custom", (req, res) => {
+  res.render('custom')
+})
+
+app.get("layouts/sedan", (req, res) => {
+  res.render('sedan')
+})
+
+app.get("layouts/sport", (req, res) => {
+  res.render('sport')
+})
+
+app.get("layouts/suv", (req, res) => {
+  res.render('suv')
+})
+app.get("layouts/truck", (req, res) => {
+  res.render('truck')
+})
+
+app.use(async (req, res, next) => {
+  next({ status: 404, message: "Sorry, we appear to have lost that page." })
+})
+
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  let message = err.status == 404 ? err.message : "Oh no! There was a crash. Maybe try a different route?"
+  res.status(err.status || 500).render("errors/error", {
+    title: err.status || "Server Error",
+    message,
+    nav,
+  })
+})
+
+const port = process.env.PORT || 5500
+const host = process.env.HOST || "localhost"
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
+  console.log(`App listening at http://${host}:${port}`)
 })
